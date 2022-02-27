4bfbea926eca4d4d9a477553a025bd83c32f8e24
patv81 <glphamhoa@gmail.com>
2022-02-27 16:35:31 +0700

    update theme genetella

---
+++ app/Http/Controllers/Admin/CouponController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\CouponModel as MainModel;
+use App\Http\Requests\CouponRequest as MainRequest;
+
+class CouponController extends AdminController
+{
+    public function __construct()
+    {
+        $this->pathViewController = 'admin.pages.coupon.';  // coupon
+        $this->controllerName     = 'coupon';
+        $this->params             = [];
+        $this->model = new MainModel();
+        $this->params["pagination"]["totalItemsPerPage"] = 5;
+        view()->share('controllerName', $this->controllerName);
+    }
+
+    public function save(MainRequest $request)
+    {
+        if ($request->method() == 'POST') {
+            $params = $request->all();
+            $params['timeStart'] = date("Y-m-d H-i-s", strtotime($params['timeStart']));
+            $params['timeEnd'] = date("Y-m-d H-i-s", strtotime($params['timeEnd']));
+            dd($params);
+
+            $task   = "add-item";
+            $notify = "Thêm phần tử thành công!";
+
+            if ($params['id'] !== null) {
+                $task   = "edit-item";
+                $notify = "Cập nhật phần tử thành công!";
+            }
+            $this->model->saveItem($params, ['task' => $task]);
+            return redirect()->route($this->controllerName)->with("zvn_notify", $notify);
+        }
+    }
+
+}
---
+++ app/Http/Requests/CouponRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CouponRequest extends FormRequest
+{
+    private $table            = 'slider';
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->id;
+
+        $condThumb = 'bail|required|image|max:500';
+        $condName  = "bail|required|between:5,100|unique:$this->table,name";
+
+        if(!empty($id)){ // edit
+            $condThumb = 'bail|image|max:500';
+            $condName  .= ",$id";
+        }
+        // return [
+        //     // 'name'        => $condName,
+        //     // 'description' => 'bail|required|min:5',
+        //     // 'link'        => 'bail|required|min:5|url',
+        //     // 'status'      => 'bail|in:active,inactive',
+        //     // 'thumb'       => $condThumb
+        //     'value_price'    =>'required_without_all:value_percent',
+        //     'value_percent'  =>'required_without_all:value_price'
+
+        // ];
+        return [
+            'value_price'=>'empty_with:value_percent',
+        ]
+    }
+
+    public function messages()
+    {
+        return [
+            // 'name.required' => 'Name không được rỗng',
+            // 'name.min'      => 'Name :input chiều dài phải có ít nhất :min ký tứ',
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            // 'description' => 'Field Description: ',
+        ];
+    }
+}
